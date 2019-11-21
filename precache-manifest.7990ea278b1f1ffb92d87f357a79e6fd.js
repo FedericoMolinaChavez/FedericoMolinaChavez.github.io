2ff9c9d0e6b3ce8f2771e037d81eeb97119d03e0
Federico <federico@Leos-iMac.local>
2019-11-21 10:40:07 -0500

    s

---
+++ precache-manifest.7990ea278b1f1ffb92d87f357a79e6fd.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654566557accc7e238423aacd42deb6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4babc99408f055bb6c7",
+    "url": "/static/css/2.06a5b0c2.chunk.css"
+  },
+  {
+    "revision": "57270e9c364119a652b7",
+    "url": "/static/css/main.34e196a1.chunk.css"
+  },
+  {
+    "revision": "b4babc99408f055bb6c7",
+    "url": "/static/js/2.74400634.chunk.js"
+  },
+  {
+    "revision": "57270e9c364119a652b7",
+    "url": "/static/js/main.6d4cbda2.chunk.js"
+  },
+  {
+    "revision": "9c94af90d30af6329adb",
+    "url": "/static/js/runtime-main.3138c030.js"
+  },
+  {
+    "revision": "17e028ee75d05d81681423c9f12b49a9",
+    "url": "/static/media/A.17e028ee.jpg"
+  },
+  {
+    "revision": "1222dacdba745d0affed53ef0e47cd71",
+    "url": "/static/media/DSC01224.1222dacd.jpg"
+  },
+  {
+    "revision": "5281e22085231246d13b1817bf146955",
+    "url": "/static/media/DSC_0012.5281e220.jpg"
+  },
+  {
+    "revision": "b0feb88df01ee27f19e5d560e78259df",
+    "url": "/static/media/DSC_0184.b0feb88d.jpg"
+  },
+  {
+    "revision": "f2f6fe8d038632298322841073a2325c",
+    "url": "/static/media/DSC_0237.f2f6fe8d.jpg"
+  },
+  {
+    "revision": "67052e9b497bb60c424a3c416d2f5979",
+    "url": "/static/media/DSC_0264.67052e9b.jpg"
+  },
+  {
+    "revision": "0f1ed066f9920b1a76dc9a6deb0552b2",
+    "url": "/static/media/DSC_0265.0f1ed066.jpg"
+  },
+  {
+    "revision": "3365f86f4cb50e271caabda6d9446d95",
+    "url": "/static/media/DSC_0267.3365f86f.jpg"
+  },
+  {
+    "revision": "bd23a21bc70c118cf9f0a72b971eb66a",
+    "url": "/static/media/DSC_0325.bd23a21b.jpg"
+  },
+  {
+    "revision": "17bfa1875fbd07c8ef885a708a404c5f",
+    "url": "/static/media/DSC_0375.17bfa187.jpg"
+  },
+  {
+    "revision": "4eac8e425c21b7db8edaeddd1881ac0d",
+    "url": "/static/media/DSC_0380.4eac8e42.jpg"
+  },
+  {
+    "revision": "eae64a4d8c9ddfbd5d5498afa9a74854",
+    "url": "/static/media/DSC_0394.eae64a4d.jpg"
+  },
+  {
+    "revision": "cfea4d4b60a18ff5d958faec35d77565",
+    "url": "/static/media/Fotos_Vacas_Web-12.cfea4d4b.jpg"
+  },
+  {
+    "revision": "e06c8d601dee40e33bee4d784febc6d7",
+    "url": "/static/media/Fotos_Vacas_Web2-11.e06c8d60.jpg"
+  },
+  {
+    "revision": "82baae104d0595ba5f12b8fa4798e062",
+    "url": "/static/media/Fotos_Vacas_Web4-06.82baae10.jpg"
+  },
+  {
+    "revision": "7388d42b0524e8cb240ff296ebc54532",
+    "url": "/static/media/Fotos_Vacasssss-03.7388d42b.jpg"
+  },
+  {
+    "revision": "f03fbffbf54db15c5bb93ebc6604f895",
+    "url": "/static/media/Fotos_Varios-10.f03fbffb.png"
+  },
+  {
+    "revision": "4688eedabf65aab70bc78dbdfea81b04",
+    "url": "/static/media/Imagenes_Pagina-25.4688eeda.png"
+  },
+  {
+    "revision": "9b5ce0706edeba2a2f11eed264a41ada",
+    "url": "/static/media/Imagenes_Pagina-29.9b5ce070.png"
+  },
+  {
+    "revision": "2edc03e95d327338ec5577262c4cde45",
+    "url": "/static/media/Vaca_1.2edc03e9.png"
+  },
+  {
+    "revision": "4bdbe1211c01039db9f16b59483c23f3",
+    "url": "/static/media/Vaca_2.4bdbe121.png"
+  },
+  {
+    "revision": "fdfcaee5f16c7696d3ec3b56cb5be7db",
+    "url": "/static/media/Vaca_3.fdfcaee5.png"
+  },
+  {
+    "revision": "9240d2c4d5354853e63ed22cdd902ea4",
+    "url": "/static/media/Vaca_4.9240d2c4.png"
+  },
+  {
+    "revision": "cac3b90593d45d6ea507286f46531338",
+    "url": "/static/media/Vaca_5.cac3b905.png"
+  },
+  {
+    "revision": "354ab118cfee67186d055e34b76856c0",
+    "url": "/static/media/Vaca_6.354ab118.png"
+  },
+  {
+    "revision": "191cea1c31fba0749d96d18ea74ca1ff",
+    "url": "/static/media/Vaca_7.191cea1c.png"
+  },
+  {
+    "revision": "3bd790793a82e444d42bbbf9c9b918e1",
+    "url": "/static/media/Vaca_introspectiva.3bd79079.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b63c82f4a85e76781686ff1c3073f5af",
+    "url": "/static/media/logo.b63c82f4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7fd73bfa8e5a84693e618d63b1a3d0b.js"
+  "/precache-manifest.7990ea278b1f1ffb92d87f357a79e6fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
